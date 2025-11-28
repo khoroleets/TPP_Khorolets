2c7636318ba48773a2af2205201c52839d691b25
khoroleets <vladkhorolets4847@gmail.com>
2025-11-28 21:38:10 +0200

    Add

--- RGR/Travel-Agency/src/main/java/travel/TravelAgencyApplication.java
+++ RGR/Travel-Agency/src/main/java/travel/TravelAgencyApplication.java
@@ -3,12 +3,11 @@ package travel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Головний клас. Анотація @SpringBootApplication:
- * 1. Налаштовує Spring Boot
- * 2. Сканує всі компоненти (контролери, репозиторії)
- * 3. Запускає вбудований веб-сервер (Tomcat)
- */
+ // Головний клас. Анотація @SpringBootApplication:
+ // 1. Налаштовує Spring Boot
+ // 2. Сканує всі компоненти (контролери, репозиторії)
+ // 3. Запускає вбудований веб-сервер (Tomcat)
+
 @SpringBootApplication
 public class TravelAgencyApplication {
 
