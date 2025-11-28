746eeba178bf3922943dee9200b29b3e78cc0231
khoroleets <vladkhorolets4847@gmail.com>
2025-11-28 21:41:43 +0200

    Add

--- RGR/Travel-Agency/src/main/java/travel/Controller/RestTravelTypeController.java
+++ RGR/Travel-Agency/src/main/java/travel/Controller/RestTravelTypeController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import travel.Model.TravelType;
 import travel.Repository.TravelTypeRepository;
 
@@ -12,16 +11,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/travel-types")
-public class TravelTypeController {
+public class RestTravelTypeController {
 
     private final TravelTypeRepository travelTypeRepository;
 
-    // Інжектуємо репозиторій через конструктор
-    public TravelTypeController(TravelTypeRepository travelTypeRepository) {
+    public RestTravelTypeController(TravelTypeRepository travelTypeRepository) {
         this.travelTypeRepository = travelTypeRepository;
     }
 
-    // Отримати список всіх типів подорожей
     @GetMapping
     public ResponseEntity<List<TravelType>> getAllTravelTypes() {
         return ResponseEntity.ok(travelTypeRepository.findAll());
