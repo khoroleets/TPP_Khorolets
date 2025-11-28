e6b97f2c0b420a4212d70da4b32a7ac75db82e40
khoroleets <vladkhorolets4847@gmail.com>
2025-11-28 21:39:21 +0200

    Add

--- RGR/Travel-Agency/src/main/java/travel/Controller/RestCountryController.java
+++ RGR/Travel-Agency/src/main/java/travel/Controller/RestCountryController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import travel.Model.Country;
 import travel.Repository.CountryRepository;
 
@@ -12,16 +11,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/countries")
-public class CountryController { 
+public class RestCountryController {
 
     private final CountryRepository countryRepository;
 
-    public CountryController(CountryRepository countryRepository) {
+    public RestCountryController(CountryRepository countryRepository) {
         this.countryRepository = countryRepository;
     }
 
     @GetMapping
-    public ResponseEntity<List<Country>> getAllCountries() { 
+    public ResponseEntity<List<Country>> getAllCountries() {
         return ResponseEntity.ok(countryRepository.findAll());
     }
 }
